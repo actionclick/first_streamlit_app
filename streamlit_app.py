68386688cc51b26e525f7f69148cb47a0ab22c93
actionclick <122148925+actionclick@users.noreply.github.com>
2023-07-11 13:52:30 +0100

    Update streamlit_app.py
    Add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
-streamlit.header("Breakfast menu")
-streamlit.text("Omega 3 & blueberry oatmeal")
-streamlit.text("Kale, spinach and rocket smoothies")
-streamlit.text("Hard Boiled free range egg")
+streamlit.header("🥣Breakfast menu")
+streamlit.text("🥗 Omega 3 & blueberry oatmeal")
+streamlit.text("🥑Kale, spinach and rocket smoothies")
+streamlit.text("🐔Hard Boiled free range egg")
