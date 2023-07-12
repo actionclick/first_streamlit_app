f9515a8d386ad1cfc63cdc1d1ab371459275b34d
actionclick <122148925+actionclick@users.noreply.github.com>
2023-07-12 11:59:59 +0100

    Update streamlit_app.py
    handle zero selection case, to display whole fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,11 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-fruit_to_show = my_fruit_list.loc[fruit_selected]
-
 # Display the table on the page.
 
-streamlit.dataframe(fruit_to_show)
+if (fruit_selected.size == 0) :
+  fruit_to_show = my_fruit_list
+else
+  fruit_to_show = my_fruit_list.loc[fruit_selected]
+  
+ streamlit.dataframe(fruit_to_show)
