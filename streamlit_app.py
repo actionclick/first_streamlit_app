0bce2b6962a96eede26b75a60902caeb4a5c5c8d
actionclick <122148925+actionclick@users.noreply.github.com>
2023-07-12 11:49:28 +0100

    Update streamlit_app.py
    show fruit list from csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit
+import pandas
 
 streamlit.title("My parents new healthy diner")
 streamlit.header("🥣Breakfast menu")
 streamlit.text("🥗 Omega 3 & blueberry oatmeal")
 streamlit.text("🥑Kale, spinach and rocket smoothies")
 streamlit.text("🐔Hard Boiled free range egg")
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
